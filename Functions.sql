c57a55d9cd5c368f0eacc0f14d44bfe2b175062c
Danihmn <daniel.bezerra.mult@gmail.com>
2026-06-13 16:40:24 -0300

    Remove funcoes e adiciona queries

--- Functions.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE FUNCTION loja.fn_alterar_loja_ativo(loja_id UUID)
-    RETURNS VOID AS
-$$
-BEGIN
-    UPDATE loja.lojas
-    SET ativo = NOT ativo
-    WHERE id = loja_id;
-END
-$$ LANGUAGE plpgsql;

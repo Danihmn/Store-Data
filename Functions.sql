3e117fc7b240b71dd7ebab9e17e65f5faa188e0c
Danihmn <daniel.bezerra.mult@gmail.com>
2026-06-15 21:11:47 -0300

    Adiciona Trigger de decrementar estoque em novos pedidos produtos

--- Functions.sql
+++ Functions.sql
@@ -7,12 +7,24 @@ BEGIN
     SELECT estoque
     INTO estoque_atual
     FROM catalogo.produtos
-    WHERE id = NEW.produdo_id;
+    WHERE id = NEW.produto_id;
 
     IF estoque_atual < NEW.quantidade THEN
         RAISE EXCEPTION 'ESTOQUE INSUFICIENTE: Disponível %, solicitado %', estoque_atual, NEW.quantidade;
     END IF;
 
     RETURN NEW;
-END ;
-$$ LANGUAGE plpgsql
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION catalogo.fn_decrementa_estoque()
+    RETURNS TRIGGER AS
+$$
+BEGIN
+    UPDATE catalogo.produtos
+    SET estoque = estoque - NEW.quantidade
+    WHERE id = NEW.produto_id;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
--- Triggers.sql
+++ Triggers.sql
@@ -1,3 +1,11 @@
 CREATE TRIGGER trg_verifica_estoque
-    BEFORE INSERT ON loja.produtos_pedidos
-    FOR EACH ROW EXECUTE FUNCTION loja.fn_valida_estoque();
+    BEFORE INSERT
+    ON loja.produtos_pedidos
+    FOR EACH ROW
+EXECUTE FUNCTION loja.fn_valida_estoque();
+
+CREATE TRIGGER trg_decrementa_estoque
+    AFTER INSERT
+    ON loja.produtos_pedidos
+    FOR EACH ROW
+EXECUTE FUNCTION catalogo.fn_decrementa_estoque();
